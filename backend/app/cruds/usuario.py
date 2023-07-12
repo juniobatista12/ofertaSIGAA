efc73af5c265b577fe95bba8c94b15fe8dd87b20
juniobatista12 <junio.batista12@gmail.com>
2023-07-12 18:40:36 -0300

    Adicionando o formulario de cadastro, entretanto, não está guardando a imagem no db

--- backend/app/cruds/usuario.py
+++ backend/app/cruds/usuario.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter, File, UploadFile
+import json
+from fastapi import APIRouter, File, HTTPException, UploadFile
 import aiofiles
 from app.models.usuario import UsuarioModel
 import app.db.main as db
@@ -11,15 +12,25 @@ router = APIRouter(
 
 @router.post("/criar-usuario")
 def criarUsuario(body: UsuarioModel):
-    listaKeys = []
-    listaValues = []
-    for campo in body:
-        listaKeys.append(campo[0])
-        listaValues.append(str(campo[1]) if type(campo[1]) == int else f'"{campo[1]}"')
-    db.executeInsert(listaValues, listaKeys, "usuarios")
+    try:
+        body
+        listaKeys = []
+        listaValues = []
+        for campo in body:
+            listaKeys.append(campo[0])
+            listaValues.append(str(campo[1]) if type(campo[1]) == int else f'"{campo[1]}"')
+        db.executeInsert(listaValues, listaKeys, "usuarios")
+        return body
+    except:
+        body = json.loads(body.json())
+        return HTTPException(
+            status_code=409,
+            detail=f'O usuário com a matrícula {body["matricula"]}'
+        )
 
 @router.post("/atualiza-foto/{matricula}")
 async def atualizaFoto(matricula: int, file: UploadFile = File(...)):
+    print(file, matricula)
     async with aiofiles.open(f"/home/{file.filename}", "wb") as fout:
         content = await file.read()
         await fout.write(content)
